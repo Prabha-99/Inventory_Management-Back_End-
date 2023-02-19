25cf70e3e5443568f64b142b759701d3109fa26a
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-02-19 11:19:18 +0530

    Initial Commit

---
+++ src/main/java/com/example/monara_backend/MonaraBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.monara_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonaraBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonaraBackEndApplication.class, args);
+	}
+
+}
