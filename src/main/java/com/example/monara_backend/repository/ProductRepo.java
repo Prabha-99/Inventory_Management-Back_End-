b1c07f176fdfefa49047d2758dd5f68eb2837ac6
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-02-24 13:34:05 +0530

    Added userSerive and userController

--- src/main/java/com/example/monara_backend/repository/ProductRepo.java
+++ src/main/java/com/example/monara_backend/repository/ProductRepo.java
@@ -4,4 +4,5 @@ import com.example.monara_backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepo extends JpaRepository<Product,String> {
+
 }
