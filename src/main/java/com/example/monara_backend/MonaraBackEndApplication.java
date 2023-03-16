e91b76e0e4fec4bed7ca86b6936dfd2a0f433440
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-03-17 01:44:24 +0530

    backend app update

--- src/main/java/com/example/monara_backend/MonaraBackEndApplication.java
+++ src/main/java/com/example/monara_backend/MonaraBackEndApplication.java
@@ -2,12 +2,36 @@ package com.example.monara_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import java.util.Collections;
+
+@SuppressWarnings("All")
 @SpringBootApplication
 public class MonaraBackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonaraBackEndApplication.class, args);
 	}
+
+	@Bean
+	public FilterRegistrationBean simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+
 
 }
