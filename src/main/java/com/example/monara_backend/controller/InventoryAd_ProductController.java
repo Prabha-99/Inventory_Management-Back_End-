dbfbde29c260d493c0f795b69a57c7702dcdb3aa
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-03-17 14:20:40 +0530

    update, get, delete successful

--- src/main/java/com/example/monara_backend/controller/InventoryAd_ProductController.java
+++ src/main/java/com/example/monara_backend/controller/InventoryAd_ProductController.java
@@ -7,10 +7,9 @@ import com.example.monara_backend.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/product")
@@ -22,7 +21,7 @@ public class InventoryAd_ProductController {
     @Autowired
     private InventoryAd_ResponseDto inventoryAd_responseDto;
 
-    @PostMapping(value = "/saveProduct")
+    @PostMapping(value = "/saveProduct")    //Add products
     public ResponseEntity saveProduct(@RequestBody InventoryAd_ProductDto inventoryAd_productDto){
         try{
             String res = inventoryAd_productService.saveProduct(inventoryAd_productDto);
@@ -48,6 +47,100 @@ public class InventoryAd_ProductController {
             inventoryAd_responseDto.setContent(null);
             return new ResponseEntity(inventoryAd_responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
     }
+
+    @PutMapping(value = "/updateProduct")
+    public ResponseEntity updateProduct(@RequestBody InventoryAd_ProductDto inventoryAd_productDto){
+
+        try{
+            String res = inventoryAd_productService.updateProduct(inventoryAd_productDto);
+            if (res.equals("00")){
+                inventoryAd_responseDto.setCode(VarList.RSP_SUCCESS);
+                inventoryAd_responseDto.setMessage("Success");
+                inventoryAd_responseDto.setContent(inventoryAd_productDto);
+                return new ResponseEntity(inventoryAd_responseDto, HttpStatus.ACCEPTED);
+
+            }else if(res.equals("01")){
+                inventoryAd_responseDto.setCode(VarList.RSP_DUPLICATED);
+                inventoryAd_responseDto.setMessage("Product is not exist");
+                inventoryAd_responseDto.setContent(inventoryAd_productDto);
+                return new ResponseEntity(inventoryAd_responseDto, HttpStatus.BAD_REQUEST);
+            }else {
+                inventoryAd_responseDto.setCode(VarList.RSP_FAIL);
+                inventoryAd_responseDto.setMessage("Error");
+                inventoryAd_responseDto.setContent(null);
+                return new ResponseEntity(inventoryAd_responseDto, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            inventoryAd_responseDto.setCode(VarList.RSP_ERROR);
+            inventoryAd_responseDto.setMessage(ex.getMessage());
+            inventoryAd_responseDto.setContent(null);
+            return new ResponseEntity(inventoryAd_responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getAllProduct") //Get all products from database
+    public ResponseEntity geAllProduct(){
+        try{
+            List<InventoryAd_ProductDto> productDtoList = inventoryAd_productService.getAllProduct();
+            inventoryAd_responseDto.setCode(VarList.RSP_SUCCESS);
+            inventoryAd_responseDto.setMessage("Success");
+            inventoryAd_responseDto.setContent(productDtoList);
+            return new ResponseEntity(inventoryAd_responseDto, HttpStatus.ACCEPTED);
+
+        }catch (Exception ex){
+            inventoryAd_responseDto.setCode(VarList.RSP_ERROR);
+            inventoryAd_responseDto.setMessage(ex.getMessage());
+            inventoryAd_responseDto.setContent(null);
+            return new ResponseEntity(inventoryAd_responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/searchProduct/{productID}")
+    public ResponseEntity searchProduct(@PathVariable int productID){
+        try{
+            InventoryAd_ProductDto inventoryAd_productDto = inventoryAd_productService.searchProduct(productID);
+            if (inventoryAd_productDto != null){
+                inventoryAd_responseDto.setCode(VarList.RSP_SUCCESS);
+                inventoryAd_responseDto.setMessage("Success");
+                inventoryAd_responseDto.setContent(inventoryAd_productDto);
+                return new ResponseEntity(inventoryAd_responseDto, HttpStatus.ACCEPTED);
+            }else {
+                inventoryAd_responseDto.setCode(VarList.RSP_DUPLICATED);
+                inventoryAd_responseDto.setMessage("Product not Available");
+                inventoryAd_responseDto.setContent(null);
+                return new ResponseEntity(inventoryAd_responseDto, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            inventoryAd_responseDto.setCode(VarList.RSP_ERROR);
+            inventoryAd_responseDto.setMessage(ex.getMessage());
+            inventoryAd_responseDto.setContent(ex);
+            return new ResponseEntity(inventoryAd_responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @DeleteMapping("/deleteProduct/{productID}")
+    public ResponseEntity deleteProduct(@PathVariable int productID){
+        try{
+            String res = inventoryAd_productService.deleteProduct(productID);
+            if (res.equals("00")){
+                inventoryAd_responseDto.setCode(VarList.RSP_SUCCESS);
+                inventoryAd_responseDto.setMessage("Successfully Deleted");
+                inventoryAd_responseDto.setContent(null);
+                return new ResponseEntity(inventoryAd_responseDto, HttpStatus.ACCEPTED);
+            }else {
+                inventoryAd_responseDto.setCode(VarList.RSP_DUPLICATED);
+                inventoryAd_responseDto.setMessage("Product not Available");
+                inventoryAd_responseDto.setContent(null);
+                return new ResponseEntity(inventoryAd_responseDto, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            inventoryAd_responseDto.setCode(VarList.RSP_ERROR);
+            inventoryAd_responseDto.setMessage(ex.getMessage());
+            inventoryAd_responseDto.setContent(ex);
+            return new ResponseEntity(inventoryAd_responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
--- src/main/java/com/example/monara_backend/service/InventoryAd_ProductService.java
+++ src/main/java/com/example/monara_backend/service/InventoryAd_ProductService.java
@@ -6,8 +6,12 @@ import com.example.monara_backend.repository.InventoryAd_ProductRepo;
 import com.example.monara_backend.util.VarList;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -20,6 +24,7 @@ public class InventoryAd_ProductService {
     @Autowired
     private ModelMapper modelMapper;
 
+    //Add products to database
     public String saveProduct(InventoryAd_ProductDto inventoryAd_productDto){
         //Check the data exist or not in the database
         if(inventoryAd_productRepo.existsById(inventoryAd_productDto.getProduct_id())){
@@ -29,5 +34,44 @@ public class InventoryAd_ProductService {
             return VarList.RSP_SUCCESS;
         }
 
+    }
+
+    //Update product in database
+    public String updateProduct(InventoryAd_ProductDto inventoryAd_productDto){
+        //Check the product exist or not
+        if(inventoryAd_productRepo.existsById(inventoryAd_productDto.getProduct_id())){
+            inventoryAd_productRepo.save(modelMapper.map(inventoryAd_productDto, Product.class));
+            return VarList.RSP_SUCCESS;
+        }else{
+            return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
+
+    //Get All products
+    public List<InventoryAd_ProductDto> getAllProduct(){
+        List<Product> productList = inventoryAd_productRepo.findAll();
+        return modelMapper.map(productList, new TypeToken<ArrayList<InventoryAd_ProductDto>>(){
+        }.getType());
+    }
+
+    // Search products
+    public InventoryAd_ProductDto searchProduct(int productId){
+        if (inventoryAd_productRepo.existsById(productId)){
+            Product product = inventoryAd_productRepo.findById(productId).orElse(null);
+            return modelMapper.map(product,  InventoryAd_ProductDto.class);
+        }else{
+            return null;
+
+        }
+    }
+
+    //Delete products
+    public String deleteProduct(int productId){
+        if (inventoryAd_productRepo.existsById(productId)){
+            inventoryAd_productRepo.deleteById(productId);
+            return VarList.RSP_SUCCESS;
+        }else{
+            return VarList.RSP_NO_DATA_FOUND;
+        }
     }
 }
