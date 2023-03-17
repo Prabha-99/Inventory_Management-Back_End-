4e1dea756272992b1ffce91007dd53c60669c20f
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-03-17 14:32:10 +0530

    updated inventoryAd_ProductController

--- src/main/java/com/example/monara_backend/controller/InventoryAd_ProductController.java
+++ src/main/java/com/example/monara_backend/controller/InventoryAd_ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/product")
 public class InventoryAd_ProductController {
