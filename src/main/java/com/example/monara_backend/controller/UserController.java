9f13eb2c8c9e15f22373732f6b97dd2c85afb4a6
KBCHANUKAJ <chanukakbc@gmail.com>
2023-04-06 15:08:13 +0530

    Update UserController.java

--- src/main/java/com/example/monara_backend/controller/UserController.java
+++ src/main/java/com/example/monara_backend/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.example.monara_backend.controller;
 
 import com.example.monara_backend.dto.UserDto;
+import com.example.monara_backend.model.User;
 import com.example.monara_backend.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -25,5 +30,20 @@ public class UserController {
         model.addAttribute("user", user);
         return "register";
     }
-    
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDto userDto,
+                               BindingResult result,
+                               Model model) {
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+        if (existingUser != null && existingUser.getUser_email() != null && !existingUser.getUser_email().isEmpty()) {
+            result.rejectValue("email", null,
+                    "Already Given Email");
+        }
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+        userService.saveUser(userDto);
+        return "redirect:/register?success";
+    }
 }
