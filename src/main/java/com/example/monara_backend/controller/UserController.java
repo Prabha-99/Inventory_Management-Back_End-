f2297d89ab540a4dc563815f327b17ae44959ca6
KBCHANUKAJ <chanukakbc@gmail.com>
2023-04-06 15:06:00 +0530

    Update UserController.java

--- src/main/java/com/example/monara_backend/controller/UserController.java
+++ src/main/java/com/example/monara_backend/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.monara_backend.controller;
 
+import com.example.monara_backend.dto.UserDto;
 import com.example.monara_backend.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -17,4 +19,11 @@ public class UserController {
     public String home() {
         return "index";
     }
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
+    }
+    
 }
