83b24a410aa89cb352b114ee13b9ed62037d9756
KBCHANUKAJ <chanukakbc@gmail.com>
2023-04-07 10:20:37 +0530

    Repo

--- src/main/java/com/example/monara_backend/model/Role.java
+++ src/main/java/com/example/monara_backend/model/Role.java
@@ -25,5 +25,7 @@ public class Role {
     @Column(length = 20)
     private ERole name;
 
-    
+    public Role(ERole name){
+        this.name=name;
+    }
 }
--- src/main/java/com/example/monara_backend/repository/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.monara_backend.repository;
-
-import com.example.monara_backend.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository <Role , Long> {
-    Role findByName(String name);
-}
