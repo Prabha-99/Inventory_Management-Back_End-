fa071f3fc2b5ba02b9c94e3ff1c846fcefad73ef
KBCHANUKAJ <chanukakbc@gmail.com>
2023-04-07 11:26:19 +0530

    Controller

--- src/main/java/com/example/monara_backend/payload/request/SignupRequest.java
+++ src/main/java/com/example/monara_backend/payload/request/SignupRequest.java
@@ -9,9 +9,7 @@ public class SignupRequest {
     @NotBlank
     private String username;
     @NotBlank
-    private String firstname;
-    @NotBlank
-    private String lastname;
+    private String name;
 
     @NotBlank
     @Email
@@ -21,4 +19,40 @@ public class SignupRequest {
 
     @NotBlank
     private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public  String getName() { return name; }
+    public  void SetName(String name) { this.name = name; }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRole() {
+        return this.role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
 }
