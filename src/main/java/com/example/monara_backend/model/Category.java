4a583d13cacecfdccd06c8c4d1ec09ca1897ee19
hasaniam <102344424+hasaniam@users.noreply.github.com>
2023-04-11 23:38:41 +0530

    Update Category.java

--- src/main/java/com/example/monara_backend/model/Category.java
+++ src/main/java/com/example/monara_backend/model/Category.java
@@ -1,4 +1,21 @@
 package com.example.monara_backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "category")
 public class Category {
+    @Id
+    private String category_id;
+    private String category_name;
+
+
 }
