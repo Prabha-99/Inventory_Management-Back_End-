ea7bef7aa190f00098bcea1cfcf23b6c5fd7c19f
hasaniam <102344424+hasaniam@users.noreply.github.com>
2023-04-24 14:55:45 +0530

    Minor Changes

--- src/main/java/com/example/monara_backend/repository/ProductRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.monara_backend.repository;
-
-import com.example.monara_backend.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product,String> {
-
-}
