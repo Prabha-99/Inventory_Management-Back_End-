d75c4de4971af3312fec32fc9fef9a0da413f99a
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-04-24 22:38:57 +0530

    User Controller Added

---
+++ src/main/java/com/example/monara_backend/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.monara_backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> welcome(){
+        return ResponseEntity.ok("Hey Man..Finally Hahh");
+    }
+}
