cdb7822384b00d4f0e3d75535b0c41cc3012870a
hasaniam <102344424+hasaniam@users.noreply.github.com>
2023-04-24 13:55:06 +0530

    Create category Dto

---
+++ src/main/java/com/example/monara_backend/dto/CategoryDto.java
@@ -0,0 +1,13 @@
+package com.example.monara_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CategoryDto {
+    private String category_id;
+    private String category_name;
+}
