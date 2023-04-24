160559fd3ca9ee98a9284e997533696842e1bf0d
KBCHANUKAJ <chanukakbc@gmail.com>
2023-04-24 23:48:04 +0530

    Update AuthenticationService.java

--- src/main/java/com/example/monara_backend/service/AuthenticationService.java
+++ src/main/java/com/example/monara_backend/service/AuthenticationService.java
@@ -1,9 +1,54 @@
 package com.example.monara_backend.service;
 
+import com.example.monara_backend.model.Role;
+import com.example.monara_backend.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user= User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepo.save(user);
+        var jwtToken=jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .Token(jwtToken)
+                .build();
+
+
+
+    }
+
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user=userRepo.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken=jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .Token(jwtToken)
+                .build();
+
+    }
 }
