eba5afba530b49338cb0f40f26f5e00f6b92cdc9
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-04-25 20:00:47 +0530

    add billsave class model

--- src/main/java/com/example/monara_backend/model/BillSave.java
+++ src/main/java/com/example/monara_backend/model/BillSave.java
@@ -1,2 +1,22 @@
-package com.example.monara_backend.model;public class BillSave {
+package com.example.monara_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name="billsave")
+public class BillSave {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "pdf_data")
+    private byte[] pdf_data;
+
+    private String name;
 }
