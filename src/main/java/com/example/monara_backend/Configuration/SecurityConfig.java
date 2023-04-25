b3e4e3bbf7e3b1d1b839118309bb4a5e9d2f2b99
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-04-25 18:28:02 +0530

    updateSecurity

--- src/main/java/com/example/monara_backend/Configuration/SecurityConfig.java
+++ src/main/java/com/example/monara_backend/Configuration/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("api/bill/getnames").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
