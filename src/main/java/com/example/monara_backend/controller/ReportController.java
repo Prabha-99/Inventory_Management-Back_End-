7669dcb8101c5e6e3c0c0274cdfc753d943bf4f5
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-04-25 19:30:32 +0530

    Autowired some classes into ReportController

--- src/main/java/com/example/monara_backend/controller/ReportController.java
+++ src/main/java/com/example/monara_backend/controller/ReportController.java
@@ -1,5 +1,8 @@
 package com.example.monara_backend.controller;
 
+import com.example.monara_backend.model.Report;
+import com.example.monara_backend.repository.ReportRepo;
+import com.example.monara_backend.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
