b75faeaa14233448516c24569cf046895e5ffa31
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-04-25 19:59:39 +0530

    update productDetailsController

--- src/main/java/com/example/monara_backend/controller/ProductDetailsController.java
+++ src/main/java/com/example/monara_backend/controller/ProductDetailsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("api/bill")
 public class ProductDetailsController {
     @Autowired
