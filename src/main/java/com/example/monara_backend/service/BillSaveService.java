3fec32525d6786485e812ec45f9648c64684e6f4
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-04-25 20:03:05 +0530

    add billsave service

--- src/main/java/com/example/monara_backend/service/BillSaveService.java
+++ src/main/java/com/example/monara_backend/service/BillSaveService.java
@@ -1,2 +1,19 @@
-package com.example.monara_backend.service;public class BillSaveService {
+package com.example.monara_backend.service;
+
+import com.example.monara_backend.model.BillSave;
+import com.example.monara_backend.repository.BillSaveRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BillSaveService {
+    @Autowired
+    private BillSaveRepo billSaveRepo;
+
+    public void savePdf(byte[] pdfData) {
+        BillSave pdf = new BillSave();
+        pdf.setPdf_data(pdfData);
+        billSaveRepo.save(pdf);
+    }
+
 }
