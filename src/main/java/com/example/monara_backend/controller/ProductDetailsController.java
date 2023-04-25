216d5467083a8f6dc2f12bb9cca72f6e68d3497f
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-04-25 18:25:39 +0530

    Add ProductDetail Controller

---
+++ src/main/java/com/example/monara_backend/controller/ProductDetailsController.java
@@ -0,0 +1,26 @@
+package com.example.monara_backend.controller;
+
+import com.example.monara_backend.service.ProductNameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api/bill")
+public class ProductDetailsController {
+    @Autowired
+    private ProductNameService productNameService;
+
+    @GetMapping("/getnames")
+    public List<String> getAllProducts() {
+        return productNameService.getAllProducts();
+    }
+
+
+
+}
