07783da2b31d6db73030e06202b6bd9e15e97375
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-04-25 19:42:45 +0530

    Changing the Access of HTTP Endpoints

--- src/main/java/com/example/monara_backend/controller/ReportController.java
+++ src/main/java/com/example/monara_backend/controller/ReportController.java
@@ -5,15 +5,15 @@ import com.example.monara_backend.repository.ReportRepo;
 import com.example.monara_backend.service.ReportService;
 import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
+@RequestMapping("/api/reports")
 public class ReportController {
 
     @Autowired
