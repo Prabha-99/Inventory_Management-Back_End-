28ec47494cb1141c799546d5490324437eafa891
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-04-26 12:33:45 +0530

    add billsave repo

--- src/main/java/com/example/monara_backend/repository/BillSaveRepo.java
+++ src/main/java/com/example/monara_backend/repository/BillSaveRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BillSaveRepo extends JpaRepository<BillSave,Long> {
+public interface BillSaveRepo extends JpaRepository<BillSave,String> {
 }
