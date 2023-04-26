71bf9215fec22b6658f038063405ed780dfed3c7
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-04-26 12:33:00 +0530

    add billsave model

--- src/main/java/com/example/monara_backend/model/BillSave.java
+++ src/main/java/com/example/monara_backend/model/BillSave.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Date;
+
 @NoArgsConstructor
 @Data
 @AllArgsConstructor
@@ -12,11 +14,24 @@ import lombok.NoArgsConstructor;
 @Table(name="billsave")
 public class BillSave {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int bill_id;
+    private String qu_no;
+    private Date st_date;
+    private Date end_date;
+    private String cu_name;
+    private String cu_address;
+    private String cu_tele;
+    private String other;
+    private double quantity;
+    private double unit_price;
+    private double amount;
+    private double discount;
+    private double subamount;
+    private double total_amount;
+    private String note;
+    private double vat;
 
-    @Column(name = "pdf_data")
-    private byte[] pdf_data;
 
-    private String name;
+
 }
