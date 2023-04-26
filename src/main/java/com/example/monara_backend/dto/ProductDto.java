181c540f26f6e82292919d43b3e29d9f0d6270ca
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-04-26 13:01:53 +0530

    updated

---
+++ src/main/java/com/example/monara_backend/dto/ProductDto.java
@@ -0,0 +1,4 @@
+package com.example.monara_backend.dto;
+
+public class ProductDto {
+}
--- src/main/java/com/example/monara_backend/repository/ProductRepo.java
+++ src/main/java/com/example/monara_backend/repository/ProductRepo.java
@@ -3,6 +3,6 @@ package com.example.monara_backend.repository;
 import com.example.monara_backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepo extends JpaRepository<Product,String> {
+public interface ProductRepo extends JpaRepository<Product,Integer> {
 
 }
