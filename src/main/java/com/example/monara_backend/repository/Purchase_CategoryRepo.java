58746adc557e802d60c67ffbe12445b0b5631c88
hasaniam <102344424+hasaniam@users.noreply.github.com>
2023-04-26 10:40:13 +0530

    minor updates

--- src/main/java/com/example/monara_backend/repository/Purchase_CategoryRepo.java
+++ src/main/java/com/example/monara_backend/repository/Purchase_CategoryRepo.java
@@ -1,7 +1,7 @@
-//package com.example.monara_backend.repository;
-//
-//import com.example.monara_backend.model.Category;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface Purchase_CategoryRepo extends JpaRepository<Category, String> {
-//}
+package com.example.monara_backend.repository;
+
+import com.example.monara_backend.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Purchase_CategoryRepo extends JpaRepository<Category, String> {
+}
