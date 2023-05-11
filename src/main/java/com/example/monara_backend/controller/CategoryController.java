3d1cd190e8980924551fb2bce011b05845e81988
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-05-11 10:17:40 +0530

    updated

--- src/main/java/com/example/monara_backend/controller/CategoryController.java
+++ src/main/java/com/example/monara_backend/controller/CategoryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("api/v1/category")
 
