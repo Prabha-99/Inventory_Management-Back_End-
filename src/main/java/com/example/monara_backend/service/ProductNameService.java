f0ec2754137b482292197148aff50a42a2836cce
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-05-12 18:34:06 +0530

    Revert "Revert "Billing system""

---
+++ src/main/java/com/example/monara_backend/service/ProductNameService.java
@@ -0,0 +1,20 @@
+package com.example.monara_backend.service;
+
+import com.example.monara_backend.model.Product;
+import com.example.monara_backend.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductNameService {
+    @Autowired
+    private ProductRepo productRepo;
+
+    public List<String> getAllProducts() {
+        List<Product> names = productRepo.findAll();
+        return names.stream().map(Product::getProduct_name).collect(Collectors.toList());
+    }
+}
