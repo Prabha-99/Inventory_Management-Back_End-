121abe6bfce560de447fe6ae384c8b18cb9b0b5e
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-05-17 00:13:09 +0530

    optimized

--- src/main/java/com/example/monara_backend/controller/PdfBillSaveController.java
+++ src/main/java/com/example/monara_backend/controller/PdfBillSaveController.java
@@ -3,18 +3,15 @@ package com.example.monara_backend.controller;
 
 import com.example.monara_backend.model.PdfBillSave;
 import com.example.monara_backend.service.PdfBillSaveService;
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
