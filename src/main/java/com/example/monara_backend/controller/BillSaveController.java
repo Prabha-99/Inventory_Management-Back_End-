f777171a56a441c1289c909b2d306edc264f961f
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-05-27 18:08:27 +0530

    changed

--- src/main/java/com/example/monara_backend/controller/BillSaveController.java
+++ src/main/java/com/example/monara_backend/controller/BillSaveController.java
@@ -13,28 +13,32 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("api/billdata")
+
 public class BillSaveController {
+
     @Autowired
     private BillSaveService billSaveService;
 
     //save billing info
     @PostMapping("/save")
     public ResponseEntity<BillSave> FormData(@RequestBody BillSave bill) {
-       BillSave savedBill = billSaveService.FormData(bill);
+        BillSave savedBill = billSaveService.FormData(bill);
         return ResponseEntity.ok(savedBill);
     }
+
 
     //get all bill info
     @GetMapping("/view")
     public List<BillSave> getAllDetails() {
         return billSaveService.getAllDetails();
     }
+    
 
-
-    //delete by id bill 
+    //delete by id bill
     @DeleteMapping("delete/{bill_id}")
     public ResponseEntity<String> deleteBill(@PathVariable Integer bill_id) {
         billSaveService.deleteBill(bill_id);
         return ResponseEntity.ok("Bill deleted");
     }
+
 }
