e437c8d6910e54b62b177ac24df994d07fafae80
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-05-27 18:13:00 +0530

    repo

--- src/main/java/com/example/monara_backend/repository/PdfBillRepo.java
+++ src/main/java/com/example/monara_backend/repository/PdfBillRepo.java
@@ -4,9 +4,7 @@ import com.example.monara_backend.model.PdfBillSave;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 
 public interface PdfBillRepo extends JpaRepository<PdfBillSave,Integer> {
 
-    
 }
