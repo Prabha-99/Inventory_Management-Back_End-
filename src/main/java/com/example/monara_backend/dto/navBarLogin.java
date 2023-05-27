e57bab8baf0b28ec06fa4f646552f11066c85587
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-05-27 22:43:43 +0530

    Added a view-model to display navbar CurrentUser

--- src/main/java/com/example/monara_backend/dto/navBarLogin.java
+++ src/main/java/com/example/monara_backend/dto/navBarLogin.java
@@ -3,11 +3,11 @@ package com.example.monara_backend.dto;
 public class navBarLogin {
 
     private String firstname;
-    private String authority;
 
-    public navBarLogin(String firstname, String authority) {
+
+    public navBarLogin(String firstname) {
         this.firstname = firstname;
-        this.authority = authority;
+
     }
 
     public String getFirstname() {
@@ -18,11 +18,5 @@ public class navBarLogin {
         this.firstname = firstname;
     }
 
-    public String getAuthority() {
-        return authority;
-    }
 
-    public void setAuthority(String authority) {
-        this.authority = authority;
-    }
 }
