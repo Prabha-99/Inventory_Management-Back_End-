43b492cfafaa256d230a8bc273eda0ee87b95f5a
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-06-19 20:09:17 +0530

    update

--- src/main/java/com/example/monara_backend/controller/ProductController.java
+++ src/main/java/com/example/monara_backend/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/product")
 
