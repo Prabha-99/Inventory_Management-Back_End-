423ae4d06c1a5f8764cefab8740c9a50de0db49d
PrasadJT <thilina1091@gmail.com>
2023-06-21 21:50:23 +0530

    updated

--- src/main/java/com/example/monara_backend/controller/ShowroomController.java
+++ src/main/java/com/example/monara_backend/controller/ShowroomController.java
@@ -3,43 +3,20 @@ import com.example.monara_backend.model.FileUpload;
 import com.example.monara_backend.service.ShowroomService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import javax.sql.rowset.serial.SerialException;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Blob;
 import java.sql.SQLException;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/showroom")
 public class ShowroomController {
 
     @Autowired
     private ShowroomService showroomService;
-
-    @GetMapping("/display")
-    public ResponseEntity<byte[]>displayFile(@RequestParam("id")long id )throws IOException,SQLException
-    {
-        File file = showroomService.viewById(id);
-        byte [] fileBytes;
-        try (InputStream inputStream = new FileInputStream(file)) {
-            fileBytes = inputStream.readAllBytes();
-        }
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_JPEG);
-        headers.setContentLength(fileBytes.length);
-
-        return new ResponseEntity<>(fileBytes, headers, HttpStatus.OK);
-    }
 
     @PostMapping("/add")
     public String addFile(HttpServletRequest request, @RequestParam("dbFile")MultipartFile file) throws IOException, SerialException, SQLException
