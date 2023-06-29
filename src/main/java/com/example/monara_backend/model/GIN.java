5a1d2048ca62438ba2a07eb3c3732c59159417a6
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-06-29 13:46:04 +0530

    Added a New Model,Service and Repository for GIN reports

---
+++ src/main/java/com/example/monara_backend/model/GIN.java
@@ -0,0 +1,41 @@
+package com.example.monara_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "GIN")
+public class GIN {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long invoice_no;
+    private Date date;
+    private String customer_name;
+    private String address;
+    private String contact_nu;
+    private String item_description;
+    private int invoiced_quantity;
+    private int issued_quantity;
+    private String remarks;
+    private String path;
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
+        return sdf.format(this.date);
+    }
+
+}
---
+++ src/main/java/com/example/monara_backend/repository/GINRepo.java
@@ -0,0 +1,7 @@
+package com.example.monara_backend.repository;
+
+import com.example.monara_backend.model.GIN;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GINRepo extends JpaRepository<GIN,Long> {
+}
