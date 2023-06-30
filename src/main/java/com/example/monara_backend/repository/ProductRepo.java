57cdeb4a96f0a2af4f088295f34e9d9211480fa6
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-06-30 13:20:59 +0530

    update

--- src/main/java/com/example/monara_backend/repository/ProductRepo.java
+++ src/main/java/com/example/monara_backend/repository/ProductRepo.java
@@ -2,9 +2,14 @@ package com.example.monara_backend.repository;
 
 import com.example.monara_backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product,Integer> {
 
+    @Modifying
+    @Query("UPDATE Product p SET p.product_quantity = p.product_quantity - :product_quantity WHERE p.product_name = :product_name AND p.product_brand = :product_brand")
+    void reduceProductQuantity(String product_name, String product_brand, int product_quantity);
 }
