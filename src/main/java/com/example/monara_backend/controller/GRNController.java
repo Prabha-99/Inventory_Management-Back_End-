c8a5052c16b4b41a338406dc8cf5de63c2ac063a
Nimesha Sewwandi <103416158+nimeshasewwandi@users.noreply.github.com>
2023-06-30 15:06:47 +0530

    GRN_data_insert

--- src/main/java/com/example/monara_backend/controller/GRNController.java
+++ src/main/java/com/example/monara_backend/controller/GRNController.java
@@ -1,23 +1,32 @@
 package com.example.monara_backend.controller;
 
+import com.example.monara_backend.model.GIN;
+import com.example.monara_backend.model.GRN;
 import com.example.monara_backend.service.GRNService;
 import lombok.RequiredArgsConstructor;
 import net.sf.jasperreports.engine.JRException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.FileNotFoundException;
 
 @RestController
 @RequestMapping("api/GRN")
-@RequiredArgsConstructor
+
 public class GRNController {
 
     private final GRNService grnService;
 
+    public GRNController(GRNService grnService) {
+        this.grnService = grnService;
+    }
+
     @GetMapping("/generateGRN")
     public String generateGRNReport() throws JRException, FileNotFoundException {
         return grnService.exportGRN();
+    }
+
+    @PostMapping("/submit")
+    public void submitGRNData(@RequestBody GRN grnData) {
+        grnService.saveGRNData(grnData);
     }
 }
