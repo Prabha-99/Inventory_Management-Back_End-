46bcfa6a3acdb4532325b0b05143d56344f3edeb
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-07-07 21:06:29 +0530

    update

--- src/main/java/com/example/monara_backend/controller/CategoryController.java
+++ src/main/java/com/example/monara_backend/controller/CategoryController.java
@@ -1,11 +1,15 @@
 package com.example.monara_backend.controller;
 
+import com.example.monara_backend.dto.CategoryDto;
+import com.example.monara_backend.dto.ProductDto;
+import com.example.monara_backend.dto.ResponseDto;
+import com.example.monara_backend.repository.CategoryRepo;
 import com.example.monara_backend.service.CategoryService;
+import com.example.monara_backend.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -15,10 +19,38 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private ResponseDto responseDto;
+
+
+
     // Beginning of the Inventory Admin
     @GetMapping("/categoryCount")
     public long getCategoryCount(){
         return categoryService.getCategoryCount();
+    }
+
+    @GetMapping("/searchCategory/{categoryID}") //Get searched product
+    public ResponseEntity searchCategory(@PathVariable int categoryID){
+        try{
+            CategoryDto categoryDto = categoryService.searchCategory(categoryID);
+            if (categoryDto != null){
+                responseDto.setCode(VarList.RSP_SUCCESS);
+                responseDto.setMessage("Success");
+                responseDto.setContent(categoryDto);
+                return new ResponseEntity(responseDto, HttpStatus.ACCEPTED);
+            }else {
+                responseDto.setCode(VarList.RSP_DUPLICATED);
+                responseDto.setMessage("Product not Available");
+                responseDto.setContent(null);
+                return new ResponseEntity(responseDto, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            responseDto.setCode(VarList.RSP_ERROR);
+            responseDto.setMessage(ex.getMessage());
+            responseDto.setContent(ex);
+            return new ResponseEntity(responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     //End of Inventory Admin
