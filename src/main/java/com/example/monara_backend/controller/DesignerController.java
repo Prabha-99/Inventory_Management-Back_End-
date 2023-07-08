01ed7665862374ccda2c5367daf1285e924ac999
PrasadJT <thilina1091@gmail.com>
2023-07-08 20:26:04 +0530

    Update DesignerController.java

--- src/main/java/com/example/monara_backend/controller/DesignerController.java
+++ src/main/java/com/example/monara_backend/controller/DesignerController.java
@@ -2,26 +2,41 @@ package com.example.monara_backend.controller;
 import com.example.monara_backend.model.DesignerBillSend;
 import com.example.monara_backend.model.Product;
 import com.example.monara_backend.model.ShowroomFile;
+
+import com.example.monara_backend.repository.ShowroomRepo;
 import com.example.monara_backend.service.DesignerBillSendService;
 import com.example.monara_backend.service.ProductService;
 import com.example.monara_backend.service.ShowroomService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.sql.rowset.serial.SerialBlob;
 import javax.sql.rowset.serial.SerialException;
+
+import java.io.File;
 import java.io.IOException;
+
+import java.nio.file.Files;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -30,6 +45,7 @@ import java.util.List;
 
 
 public class DesignerController {
+
     @Autowired
     private ShowroomService showroomService;
 
@@ -38,6 +54,10 @@ public class DesignerController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private ShowroomRepo showroomRepo;
+
 
     @GetMapping("/files")
 
@@ -53,6 +73,22 @@ public class DesignerController {
             objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
             return objectMapper;
         }
+    }
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> downloadFile(@RequestParam Integer id) throws SQLException {
+        ShowroomFile file = showroomService.getFileById(id);
+        if (file == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // Set the response headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+
+        // Stream the file content to the response
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(file.getDbFile().getBytes(1, (int) file.getDbFile().length()));
     }
 
     @PostMapping("/billSend")
