fccb4759658f444cd8915fee66168c76239ac96d
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-07-14 19:05:05 +0530

    Fixed some bugs in Report Generation Modules

--- src/main/java/com/example/monara_backend/repository/GINRepo.java
+++ src/main/java/com/example/monara_backend/repository/GINRepo.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface GINRepo extends JpaRepository<GIN,Long> {
-    @Query(value = "SELECT * FROM gin WHERE TIME(date) >= CURTIME() ORDER BY date DESC LIMIT 1;", nativeQuery = true)//Getting the Newest GIN
+    @Query(value = "SELECT * FROM gin WHERE TIME(date) <= CURTIME() ORDER BY date DESC LIMIT 1;", nativeQuery = true)//Getting the Newest GIN
     List<GIN> newestGIN();
 
 //    @Query(value = "SELECT path FROM reports WHERE report_name LIKE 'GIN%' ORDER BY date DESC LIMIT 1;", nativeQuery = true)//Getting the path of the Newest GIN
