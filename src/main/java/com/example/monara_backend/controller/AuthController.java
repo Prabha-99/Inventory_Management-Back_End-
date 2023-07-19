779e01ebc42a6fc4925db0f163eddacd6745c3c3
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-07-19 11:10:26 +0530

    Nothing Changed

--- src/main/java/com/example/monara_backend/controller/AuthController.java
+++ src/main/java/com/example/monara_backend/controller/AuthController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
