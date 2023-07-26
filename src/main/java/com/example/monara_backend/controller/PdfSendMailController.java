6c01b1bebe97be99624d96b3270bc82cf1188c79
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-07-26 10:46:28 +0530

    cross change

--- src/main/java/com/example/monara_backend/controller/PdfSendMailController.java
+++ src/main/java/com/example/monara_backend/controller/PdfSendMailController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
 @RequestMapping("/api/billmail")
 public class PdfSendMailController {
 
